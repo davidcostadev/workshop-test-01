9519d69d2c7aec527ad099af185066bbaf5680b0
David Costa <davidcostadev@gmail.com>
2019-03-22 10:57:41 -0300

    Fixing eslint

--- src/left-roration-[yourname].js
+++ src/left-roration-[yourname].js
@@ -1,4 +1,11 @@
-// function
+/**
+ * function
+ *
+ * @param {int} quant
+ * @param {int} rotation
+ * @param {array} arr
+ */
+// eslint-disable-next-line no-unused-vars
 function leftRotation(quant, rotation, arr) {
 
 }
