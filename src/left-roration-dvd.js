feb750f5fab246a009a5bccd1f95153464b43721
David Marquardt <david.marquardt@evolux.net.br>
2019-03-28 11:09:05 -0300

    add space after for

--- src/left-roration-dvd.js
+++ src/left-roration-dvd.js
@@ -1,6 +1,6 @@
 // function
 function leftRotation(quant, rotation, arr) {
-  for(let i = 1; i <= rotation; i++) {
+  for (let i = 1; i <= rotation; i++) {
     const firstElement = arr.shift();
     arr.push(firstElement);
   }
