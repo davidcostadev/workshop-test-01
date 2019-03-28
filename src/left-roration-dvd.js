7e5e47cda5a658eddbd3a717f7b586d3578a2a13
David Marquardt <david.marquardt@evolux.net.br>
2019-03-28 13:27:21 -0300

    stop eslint happen

--- src/left-roration-dvd.js
+++ src/left-roration-dvd.js
@@ -1,6 +1,6 @@
 // function
 function leftRotation(quant, rotation, arr) {
-  for (let i = 1; i <= rotation; i++) {
+  for (let i = 1; i <= rotation; i = i + 1) {
     const firstElement = arr.shift();
     arr.push(firstElement);
   }
